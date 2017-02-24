42d679fd6b33afe882d06f3885da8506c35d20c3
jasper ginn <jasperginn@gmail.com>
2017-02-24 03:41:22 -0500

    Postgres credentials

--- python/convert_ondemand_config.py
+++ python/convert_ondemand_config.py
@@ -10,6 +10,6 @@ postgres_database_name = 'realtimesystems'
 
 ##Valid in both launcher and standalone
 postgres_host = '127.0.0.1'
-postgres_user = 'postgres'
-postgres_pwd = 'muthu'
+postgres_user = 'shiny'
+postgres_pwd = 'root'
 delimiter = ','
